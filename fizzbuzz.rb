fbffafa0b817049f094de035ddcc1a4c36a04c85
Skye Geraghty <skyetay03@googlemail.com>
2020-01-06 07:11:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,5 +3,5 @@
 
 
 def fizzbuzz(num)
-  return num % 3 == 0 && num % 5 == 0 ? "FizzBuzz" : num % 3 == 0 ? "Fizz" : num % 5 == 0 ? "Buzz" : 
+  return num % 3 == 0 && num % 5 == 0 ? "FizzBuzz" : num % 3 == 0 ? "Fizz" : num % 5 == 0 ? "Buzz" : nil
 end
